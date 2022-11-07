6b3c04ccbb8fe4397584a7a0ccfb311cad311dfa
oskar-waldoch <oskar.waldoch200@gmail.com>
2022-11-07 17:07:58 +0100

    Created Class Grid

--- grid.py
+++ grid.py
@@ -1,3 +1,18 @@
-class Cell:
-    def __init__(self):
-        self.clicked = False
+import pygame 
+class Grid:
+
+    def __init__(self, size):
+        self.size = size
+
+    def createGrid(self, screen, gridSize, gridMargin, gridWidth, gridHeight, color):
+        grid = []
+
+        for row in range(gridSize):
+            grid.append([])
+            for column in range(gridSize):
+                grid[row].append(0)
+
+        for row in range(gridSize):
+            for column in range(gridSize):
+                pygame.draw.rect(screen, color, [(gridMargin + gridWidth) * column + gridMargin,(gridMargin + gridHeight) * row + gridMargin,gridWidth,gridHeight])
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 import zwierze
 import random
-
+from grid import Grid
 
 gridSize = int(input("Podaj "))
 
@@ -15,9 +15,12 @@ WIN = pygame.display.set_mode((WIDTH1, HEIGHT1))
 # Title
 pygame.display.set_caption("Wirtualny Swiat")
 
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+
 # Clock
 clock = pygame.time.Clock()
-
 
 # MainGame window
 game = pygame.Surface((420, 420))
@@ -28,21 +31,15 @@ GRID_MARGIN = 1
 GRID_WIDTH = (game.get_width() - (gridSize * GRID_MARGIN)) / gridSize
 GRID_HEIGHT = (game.get_height() - (gridSize * GRID_MARGIN)) / gridSize
 
-# Create a 2 dimensional array
-grid = []
-
-for row in range(gridSize):
-    grid.append([])
-    for column in range(gridSize):
-        grid[row].append(0)
-
-
 
 ####grid[wilk1.x][wilk1.y] = wilk1
+
+grid1 = Grid(gridSize)
 
 
 def main():
 
+    grid1.createGrid(game, gridSize, GRID_MARGIN, GRID_WIDTH, GRID_HEIGHT, white)
 
     while True:
         dt = clock.tick()
@@ -52,13 +49,7 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-         # Draw the grid
-        for row in range(gridSize):
-            for column in range(gridSize):
-                color = (255,255,255)
-                if grid[row][column] == 1:
-                    color = (255, 255, 255)
-                pygame.draw.rect(game,color,[(GRID_MARGIN + GRID_WIDTH) * column + GRID_MARGIN,(GRID_MARGIN + GRID_HEIGHT) * row + GRID_MARGIN,GRID_WIDTH,GRID_HEIGHT])
+        
         WIN.blit(game, game.get_rect(center = WIN.get_rect().center))
 
 
