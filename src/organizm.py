b4e037cd3ff0551db4a531d12bb9f0ce8f7e97c0
oskar-waldoch <oskar.waldoch200@gmail.com>
2022-11-14 21:24:50 +0100

    Update stuff

--- src/organizm.py
+++ src/organizm.py
@@ -8,8 +8,10 @@ class Organizm(Swiat):
     inicjatywa = 0
     pos_x = 0
     pos_y = 0
+    typ = ""
 
-    def __init__(self, sila, inicjatywa, pos_x, pos_y):
+    def __init__(self, typ, sila, inicjatywa, pos_x, pos_y):
+        self.typ = typ
         self.sila = sila
         self.inicjatywa = inicjatywa
         self.pos_x = pos_x
@@ -20,14 +22,14 @@ class Organizm(Swiat):
 
         if Organizm.pos_x == 19:
             move = random.choice([2, 3, 4])
+        elif Organizm.pos_y == 0 and Organizm.pos_x == 0:
+            move = random.choice([1, 2])
         elif Organizm.pos_y == 19:
             move = random.choice([1, 2, 3])
         elif Organizm.pos_x == 0:
             move = random.choice([1, 2, 4])
         elif Organizm.pos_y == 0:
             move = random.choice([1, 3, 4])
-        elif Organizm.pos_y == 0 and Organizm.pos_x:
-            move = random.choice([1, 2])
         else: 
             move = random.randrange(1,4)
 
@@ -40,5 +42,39 @@ class Organizm(Swiat):
                 Organizm.pos_x -= 1
             case 4:
                 Organizm.pos_y += 1
+
+
+    def kolizja(self, other):
+
+        if self.pos_x == other.pos_x and self.pos_y == other.pos_y:
+            print("ta sama pozycja")
+            if self.sila > other.sila:
+                print(str(self.typ), "jest lepszy od", str(other.typ))
+            elif self.sila < other.sila:
+                print(str(self.typ), "jest słabszy od", str(other.typ))
+            
+
+
+
+        '''
+        if len(Swiat.organisms) > 1:
+            if self.pos_x == other.pos_x and self.pos_y == other.pos_y:
+                if self.sila > other.sila:
+                    Grid.gridList[self.pos_x][self.pos_y] = self.typ
+                    print("Usunięto słabszego - 1")
+                    Swiat.organisms.remove(other)
+                else :
+                        Swiat.organisms.remove(self)
+                        Grid.gridList[other.pos_x][other.pos_y] = other.typ
+                        print(other.typ)
+                        print("usunięto słabszego - 2")
+            else:
+                Grid.gridList[self.pos_x][self.pos_y] = self.typ
+                Grid.gridList[other.pos_x][other.pos_y] = other.typ
+                print("usunięto nikogo - 3")
+        else:
+            Grid.gridList[self.pos_x][self.pos_y] = self.typ
+        '''
+
 
 
